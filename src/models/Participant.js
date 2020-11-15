866d40d7d328c41c082e7e13995c926305f4ca5a
nattanon <nattanon@hospital-os.com>
2020-11-15 14:44:14 +0700

    Test extends

--- src/models/Participant.js
+++ src/models/Participant.js
@@ -1,10 +1,13 @@
 import Block from './Block'
 export default class ParticipantBlock extends Block {
-  // constructor(index, previousHash, timestamp, data, hash) {
-  // this.index = index;
-  // this.previousHash = previousHash.toString();
-  // this.timestamp = timestamp;
-  // this.hash = hash.toString();
-  // this.data = data
-  // }
+  constructor(index, previousHash, timestamp, data, hash) {
+    // test extends
+
+    super(index, previousHash, timestamp, data, hash)
+    // this.index = index;
+    // this.previousHash = previousHash.toString();
+    // this.timestamp = timestamp;
+    // this.hash = hash.toString();
+    // this.data = data
+  }
 }
