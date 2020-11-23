7082e449b9c001329d4a24ac82fa5f60d6ad1406
nattanon <nattanon@hospital-os.com>
2020-11-23 22:44:14 +0700

    add logic

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import WebSocket from 'ws'
 import models from './src/models'
+import { gardenEMRId } from './src/helpers/logic'
 import blockchainsLogic, { getGenesisEMRBlock } from './src/blockchain/logic'
 import middleware, { updateToken } from './middleware'
 import { write, broadcast, initErrorHandler, initMessageHandler } from './src/socket_servers'
@@ -11,18 +12,29 @@ const http_port = process.env.HTTP_PORT || 3001;
 const p2p_port = process.env.P2P_PORT || 6001;
 const initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 let sockets = [];
-
+let emr_id = 1;
 let EMRBlockchain = [getGenesisEMRBlock()];
 
+const updateEMRId = (new_id) => { emr_id = new_id };
 const initHttpServer = () => {
   const app = express();
   app.use(bodyParser.json());
+
+  app.post('api/nurse/admit/new/:hn', middleware, (req, res) => {
+    const newBlock = blockchainsLogic.generateNextBlock(crypto.encryption(gardenEMRId(emr_id, updateEMRId, req.body.data)), EMRBlockchain, EMRBlock);
+
+    EMRBlockchain = blockchainsLogic.addBlock(newBlock, EMRBlockchain)
+    broadcast(sockets, blockchainsLogic.responseLatestMsg(EMRBlockchain));
+    console.log('block added: ' + JSON.stringify(newBlock));
+    res.send(JSON.stringify(newBlock));
+  });
 
   app.get('/EMRs', middleware, (req, res) => res.send(JSON.stringify(EMRBlockchain)));
   app.get('/EMRs/:index', middleware, (req, res) => {
     const b = EMRBlockchain[req.params.index]
     res.send(JSON.stringify(new EMRBlock(b['index'], b['previousHash'], b['timestamp'], b['data'], b['hash'])))
   });
+
   app.post('/mineEMR', middleware, (req, res) => {
     const newBlock = blockchainsLogic.generateNextBlock(crypto.encryption(req.body.data), EMRBlockchain, EMRBlock);
 
