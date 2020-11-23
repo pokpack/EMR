87bd26b5627bf4e3810dfcfb7c8bd246a161e841
nattanon <nattanon@hospital-os.com>
2020-11-23 23:09:56 +0700

    Add fix

--- src/blockchain/logic.js
+++ src/blockchain/logic.js
@@ -104,7 +104,7 @@ const getGenesisEMRBlock = () => {
   return {
     "index": 0,
     "previousHash": `0`,
-    "timestamp": Date.now(),
+    "timestamp": 1606147699953,
     "hash": `816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7`,
     "data": `U2FsdGVkX19rnP7i0ysECsV/l7QeKI8oN7oXOyM4vN4=`
   };
