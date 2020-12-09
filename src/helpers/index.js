ca5f91f2c72f57061a66da026f9b18fac99c9b38
nattanon <nattanon@hospital-os.com>
2020-12-09 11:28:24 +0700

    Update config nginx

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,33 +1,24 @@
 import crypto from './crypto'
 import fsmanager from './fsmanager'
 const timeDifference = (date1, date2) => {
-  var difference = date1.getTime() - date2.getTime();
-
-  var daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
+  const difference_init = date1.getTime() - date2.getTime();
+  let difference = difference_init
+  let daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
   difference -= daysDifference * 1000 * 60 * 60 * 24
 
-  var hoursDifference = Math.floor(difference / 1000 / 60 / 60);
+  let hoursDifference = Math.floor(difference / 1000 / 60 / 60);
   difference -= hoursDifference * 1000 * 60 * 60
 
-  var minutesDifference = Math.floor(difference / 1000 / 60);
+  let minutesDifference = Math.floor(difference / 1000 / 60);
   difference -= minutesDifference * 1000 * 60
 
-  var secondsDifference = Math.floor(difference / 1000);
-  console.log("===========secondsDifference===========")
-  console.log(`sent = ${date1}`, `receive = ${date2}`)
-  console.log('difference = ' +
-    daysDifference + ' day/s ' +
-    hoursDifference + ' hour/s ' +
-    minutesDifference + ' minute/s ' +
-    secondsDifference + ' second/s ' +
-    difference + 's');
-  console.log("=======================================")
+  let secondsDifference = Math.floor(difference / 1000);
+  const difference_s = `${daysDifference + ' day/s '} ${hoursDifference + ' hour/s '} ${minutesDifference + ' minute/s '} ${secondsDifference + ' second/s '} ${difference_init + ' second/s '}`
+  console.log("==TEST== +-:-> seconds difference : ", difference_s)
 }
 
 export const checkDataSize = data => {
-  console.log("===========MessageByteSize===========")
-  console.log(Buffer.byteLength(data, 'utf8'), 'byte')
-  console.log("=====================================")
+  console.log("==TEST== +-:-> message byte size : ", Buffer.byteLength(data, 'utf8'), 'byte')
 }
 export const checkdifference = sent_date => {
   timeDifference(new Date(Date.now()), sent_date)
